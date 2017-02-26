8e15b8db4224777426675e3f1f8cd71552290d18
Madoka Machitani <madokama@gmail.com>
2017-02-26 11:23:00 +0900

    elshogi-candidates.el: rename functions to conform to package name

--- lisp/elshogi/elshogi-candidates.el
+++ lisp/elshogi/elshogi-candidates.el
@@ -9,34 +9,36 @@
 (require 'elshogi-directions)
 (require 'dash-functional)
 
-(defun elshogi-move-candidates-pawn (index piece)
+(defun elshogi-candidates-move-pawn (index piece)
   (list (elshogi-upward-index index piece)))
 
-(defun elshogi-move--directions (index piece directions)
+(defun elshogi-candidates-move-directions (index piece directions)
   (mapcar (lambda (direction)
             (funcall direction index piece))
           directions))
 
-(defun elshogi-move-candidates-knight (index piece)
-  (elshogi-move--directions (elshogi-upward-index index piece)
-                            piece
-                            '(elshogi-upleft-index elshogi-upright-index)))
+(defun elshogi-candidates-move-knight (index piece)
+  (elshogi-candidates-move-directions
+   (elshogi-upward-index index piece)
+   piece
+   '(elshogi-upleft-index elshogi-upright-index)))
 
-(defun elshogi-move-candidates-silver (index piece)
-  (elshogi-move--directions index piece
-                            (cons 'elshogi-upward-index elshogi-directions-x)))
+(defun elshogi-candidates-move-silver (index piece)
+  (elshogi-candidates-move-directions
+   index piece
+   (cons 'elshogi-upward-index elshogi-directions-x)))
 
-(defun elshogi-move-candidates-gold (index piece)
-  (elshogi-move--directions index piece
-                            (append elshogi-directions-+
-                                    '(elshogi-upleft-index
-                                      elshogi-upright-index))))
+(defun elshogi-candidates-move-gold (index piece)
+  (elshogi-candidates-move-directions
+   index piece
+   (append elshogi-directions-+ '(elshogi-upleft-index elshogi-upright-index))))
 
-(defun elshogi-move-candidates-king (index piece)
-  (elshogi-move--directions index piece
-                            (append elshogi-directions-+ elshogi-directions-x)))
+(defun elshogi-candidates-move-king (index piece)
+  (elshogi-candidates-move-directions
+   index piece
+   (append elshogi-directions-+ elshogi-directions-x)))
 
-(defun elshogi-move-candidates--ranging (index piece dir)
+(defun elshogi-candidates-move--ranging (index piece dir)
   (cl-loop for index~ = (funcall dir index piece) then (funcall dir index~ piece)
            while index~
            collect index~ into candidates
@@ -44,24 +46,24 @@
              return candidates
            finally return candidates))
 
-(defun elshogi-move-candidates-lance (index piece)
-  (elshogi-move-candidates--ranging index piece #'elshogi-upward-index))
+(defun elshogi-candidates-move-lance (index piece)
+  (elshogi-candidates-move--ranging index piece #'elshogi-upward-index))
 
-(defun elshogi-move-candidates-rook (index piece)
-  (append (cl-mapcan (apply-partially #'elshogi-move-candidates--ranging
+(defun elshogi-candidates-move-rook (index piece)
+  (append (cl-mapcan (apply-partially #'elshogi-candidates-move--ranging
                                       index piece)
                      elshogi-directions-+)
           (when (elshogi-piece/promoted piece)
-            (elshogi-move--directions index piece elshogi-directions-x))))
+            (elshogi-candidates-move-directions index piece elshogi-directions-x))))
 
-(defun elshogi-move-candidates-bishop (index piece)
-  (append (cl-mapcan (apply-partially #'elshogi-move-candidates--ranging
+(defun elshogi-candidates-move-bishop (index piece)
+  (append (cl-mapcan (apply-partially #'elshogi-candidates-move--ranging
                                       index piece)
                      elshogi-directions-x)
           (when (elshogi-piece/promoted piece)
-            (elshogi-move--directions index piece elshogi-directions-+))))
+            (elshogi-candidates-move-directions index piece elshogi-directions-+))))
 
-(defun elshogi-file-with-pawn-p (file piece)
+(defun elshogi-candidates-file-with-pawn-p (file piece)
   (cl-loop for rank below 9
            for piece~ = (elshogi-piece-at elshogi-current-game
                                           (elshogi-calc-index file rank))
@@ -70,12 +72,13 @@
                    (not (elshogi-piece/promoted piece~)))
              return t))
 
-(defun elshogi-drop-candidates-pawn (piece candidates)
+(defun elshogi-candidates-drop-pawn (piece candidates)
   (seq-remove (lambda (index)
-                (elshogi-file-with-pawn-p (elshogi-index-file index) piece))
-              (elshogi-drop-candidates-lance piece candidates)))
+                (elshogi-candidates-file-with-pawn-p (elshogi-index-file index)
+                                                     piece))
+              (elshogi-candidates-drop-lance piece candidates)))
 
-(defun elshogi-drop-candidates-lance (piece candidates)
+(defun elshogi-candidates-drop-lance (piece candidates)
   (let ((illegal-rank
          (if (elshogi-piece-black-p piece)
              0
@@ -84,7 +87,7 @@
                   (= (elshogi-index-rank index) illegal-rank))
                 candidates)))
 
-(defun elshogi-drop-candidates-knight (piece candidates)
+(defun elshogi-candidates-drop-knight (piece candidates)
   (let ((illegal-p
          (if (elshogi-piece-black-p piece)
              (lambda (rank)
@@ -94,18 +97,18 @@
     (seq-remove (-compose illegal-p #'elshogi-index-rank)
                 candidates)))
 
-(defun elshogi-drop-candidates (piece)
+(defun elshogi-candidates-drop (piece)
   (let ((candidates
          (elshogi-collect-indices (-not
                                    (apply-partially #'elshogi-piece-at
                                                     elshogi-current-game)))))
     (cl-case (elshogi-piece/name piece)
-      (p (elshogi-drop-candidates-pawn piece candidates))
-      (l (elshogi-drop-candidates-lance piece candidates))
-      (n (elshogi-drop-candidates-knight piece candidates))
+      (p (elshogi-candidates-drop-pawn piece candidates))
+      (l (elshogi-candidates-drop-lance piece candidates))
+      (n (elshogi-candidates-drop-knight piece candidates))
       (t candidates))))
 
-(defun elshogi-move-candidates--legal (piece candidates)
+(defun elshogi-candidates-move--legal (piece candidates)
   (let ((candidates~
          (seq-remove (lambda (index)
                        (or (not index)
@@ -116,36 +119,36 @@
                      candidates)))
     (if (eq (elshogi-piece/name piece) 'k)
         (seq-difference candidates~
-                        (cl-mapcan #'elshogi-move-candidates--raw
+                        (cl-mapcan #'elshogi-candidates-move--raw
                                    (elshogi-indices-on-board
                                     elshogi-current-game
                                     (elshogi-negate-side
                                      (elshogi-piece/side piece)))))
       candidates~)))
 
-(defun elshogi-move-candidates--raw (index)
+(defun elshogi-candidates-move--raw (index)
   (let* ((piece (elshogi-piece-at elshogi-current-game index))
          (name (elshogi-piece/name piece)))
     (funcall (if (and (elshogi-piece/promoted piece)
                       (memq name '(p l n s)))
-                 #'elshogi-move-candidates-gold
+                 #'elshogi-candidates-move-gold
                (cl-case name
-                 (p #'elshogi-move-candidates-pawn)
-                 (l #'elshogi-move-candidates-lance)
-                 (n #'elshogi-move-candidates-knight)
-                 (s #'elshogi-move-candidates-silver)
-                 (g #'elshogi-move-candidates-gold)
-                 (b #'elshogi-move-candidates-bishop)
-                 (r #'elshogi-move-candidates-rook)
-                 (k #'elshogi-move-candidates-king)))
+                 (p #'elshogi-candidates-move-pawn)
+                 (l #'elshogi-candidates-move-lance)
+                 (n #'elshogi-candidates-move-knight)
+                 (s #'elshogi-candidates-move-silver)
+                 (g #'elshogi-candidates-move-gold)
+                 (b #'elshogi-candidates-move-bishop)
+                 (r #'elshogi-candidates-move-rook)
+                 (k #'elshogi-candidates-move-king)))
              index piece)))
 
 (defun elshogi-candidates-target (index)
   (if (consp index)
-      (elshogi-drop-candidates (cdr (assq 'piece index)))
-    (elshogi-move-candidates--legal (elshogi-piece-at elshogi-current-game
+      (elshogi-candidates-drop (cdr (assq 'piece index)))
+    (elshogi-candidates-move--legal (elshogi-piece-at elshogi-current-game
                                                       index)
-                                    (elshogi-move-candidates--raw index))))
+                                    (elshogi-candidates-move--raw index))))
 
 (defun elshogi-candidates-origin (file)
   (cl-loop for rank below 9
