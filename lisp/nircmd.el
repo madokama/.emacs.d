644174db52eb442c5d9e28cc16a220b03dea4679
Madoka Machitani <madokama@gmail.com>
2017-10-07 07:06:41 +0900

    lisp/nircmd.el: New.

---
+++ lisp/nircmd.el
@@ -0,0 +1,22 @@
+;;; nircmd --- Misc. system commands interface to MS-Windows -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(defsubst nircmd--execute (&rest cmd)
+  (unless (zerop (apply #'call-process "nircmd" nil nil nil cmd))
+    (error "[nircmd] %s failed (params: %s)" (car cmd) (cdr cmd))))
+
+;;;###autoload
+(defun nircmd-activate-process (pid)
+  (nircmd--execute "win" "activate" "process" (format "/%d" pid)))
+
+;;;###autoload
+(defun nircmd-send-key (pid key)
+  "Send input KEY to the process PID."
+  (nircmd-activate-process pid)
+  (nircmd--execute "sendkeypress" key))
+
+(provide 'nircmd)
+;;; nircmd.el ends here
