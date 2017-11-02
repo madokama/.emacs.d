c4bac627d77817cd73ad267d439b006d4a7c71ae
Madoka Machitani <madokama@gmail.com>
2017-11-02 19:43:51 +0900

    lisp/firefox.el: Major refactoring.

--- lisp/firefox.el
+++ lisp/firefox.el
@@ -4,6 +4,11 @@
 
 ;;; Code:
 
+(eval-when-compile
+  (require 'subr-x))
+(require 'async)
+(require 'json)
+
 (defgroup firefox nil
   "Firefox utilities."
   :prefix "firefox-"
@@ -12,43 +17,47 @@
 
 (defvar firefox-profile-directory nil)
 
+(defun firefox-mozlz4 (file)
+  (with-temp-buffer
+    (insert "import sys, lz4\n"
+            "out = sys.stdout\n"
+            (format "f = open(%S, 'rb')\n" file)
+            "magic = f.read(8)\n"
+            "out.write(lz4.block.decompress(f.read()).decode('utf-8'))\n"
+            "f.close()\n"
+            "out.flush()\n")
+    (call-process-region (point-min) (point-max) "python3" t t nil)
+    (goto-char (point-min))
+    (json-read)))
+
+(defun firefox-session-tabs (json)
+  (cl-flet ((normalize (url)
+              (if (string-match-p "\\`moz-extension://" url)
+                  (car
+                   (assoc-default "url" (url-parse-query-string url)
+                                  #'string=))
+                url)))
+    (thread-last json
+      (alist-get 'windows)
+      (mapcan (lambda (window)
+                (mapcar (lambda (tab)
+                          (let-alist tab
+                            (cons .lastAccessed
+                                  (let-alist (aref .entries (1- .index))
+                                    (cons (normalize .url) .title)))))
+                        (alist-get 'tabs window))))
+      (seq-sort-by #'car #'>))))
+
 (defun firefox-tabs (callback)
   ;; Reference: https://unix.stackexchange.com/a/389360
   (async-start
    `(lambda ()
-      (require 'subr-x)
-      (require 'seq)
-      (require 'json)
-      
-      (with-temp-buffer
-        (insert "import sys, lz4\n"
-                "out = sys.stdout\n"
-                (format "f = open(%S, 'rb')\n"
-                        ,(expand-file-name "sessionstore-backups/recovery.jsonlz4"
-                                           firefox-profile-directory))
-                "magic = f.read(8)\n"
-                "out.write(lz4.block.decompress(f.read()).decode('utf-8'))\n"
-                "f.close()\n"
-                "out.write('\\n')\n"
-                "out.flush()\n")
-        (call-process-region (point-min) (point-max) "python3" t t nil)
-        (goto-char (point-min))
-        (cl-flet ((normalize (url)
-                    (if (string-match-p "\\`moz-extension://" url)
-                        (car
-                         (assoc-default "url" (url-parse-query-string url)
-                                        #'string=))
-                      url)))
-          (thread-last (json-read)
-            (alist-get 'windows)
-            (mapcan (lambda (window)
-                      (mapcar (lambda (tab)
-                                (let-alist tab
-                                  (cons .lastAccessed
-                                        (let-alist (aref .entries (1- .index))
-                                          (cons (normalize .url) .title)))))
-                              (alist-get 'tabs window))))
-            (seq-sort-by #'car #'>)))))
+      ,(async-inject-variables "\\`load-path\\'")
+      (require 'firefox)
+      (firefox-session-tabs
+       (firefox-mozlz4
+        (expand-file-name "sessionstore-backups/recovery.jsonlz4"
+                          ,firefox-profile-directory))))
    callback))
 
 (provide 'firefox)
