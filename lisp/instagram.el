90b3cccba8e3fdda91281c1beb2e730a936ae6a1
Madoka Machitani <madokama@gmail.com>
2017-12-12 19:28:34 +0900

    lisp/instagram.el: Use Jansson-based JSON functions.

--- lisp/instagram.el
+++ lisp/instagram.el
@@ -6,7 +6,6 @@
 
 (eval-when-compile
   (require 'subr-x))
-(require 'json)
 (require 'xml)
 
 (autoload 'url-curl-extract-cookies "url-curl")
@@ -19,7 +18,7 @@
 (defun instagram-user-id ()
   (goto-char (point-min))
   (when (search-forward "window._sharedData = " nil t)
-    (let-alist (aref (let-alist (json-read)
+    (let-hash (aref (let-hash (json-parse-buffer)
                        .entry_data.ProfilePage)
                      0)
       .user.id)))
@@ -47,18 +46,18 @@
                          "authority: i.instagram.com"
                          (concat "cookie: " credentials))))
     (goto-char (point-min))
-    (json-read)))
+    (json-parse-buffer)))
 
 ;;; IG Private APIs
 
 (defun instagram-user-feed (user-id credentials)
-  (alist-get 'items
-             (instagram-json-api (instagram-api/user-feed nil user-id)
-                                 credentials)))
+  (let-hash (instagram-json-api (instagram-api/user-feed nil user-id)
+                                credentials)
+    .items))
 
 (defun instagram-user-story (user-id credentials)
-  (let-alist (instagram-json-api (instagram-api/user-feed "story/" user-id)
-                                 credentials)
+  (let-hash (instagram-json-api (instagram-api/user-feed "story/" user-id)
+                                credentials)
     .reel.items))
 
 ;;; IG Public APIs
@@ -77,17 +76,17 @@
                      "Referer: https://www.instagram.com"
                      "x-requested-with: XMLHttpRequest")))
     (goto-char (point-min))
-    (let-alist (json-read)
+    (let-hash (json-parse-buffer)
       .graphql.shortcode_media)))
 
 (defun instagram-shortcode-media (json)
-  (let-alist json
+  (let-hash json
     (pcase .__typename
       ("GraphImage" .display_url)
       ("GraphSidecar"
        (mapcar (lambda (node)
-                 (let-alist (alist-get 'node node)
-                   .display_url))
+                 (let-hash node
+                   .node.display_url))
                .edge_sidecar_to_children.edges))
       ("GraphVideo"
        (list .video_url .display_url)))))
@@ -99,7 +98,7 @@
 ;;; Media item APIs
 
 (defsubst instagram--media-type (item)
-  (alist-get 'media_type item))
+  (gethash "media_type" item))
 
 (defun instagram-item-video-p (item)
   (= (instagram--media-type item) 2))
@@ -108,30 +107,30 @@
   (= (instagram--media-type item) 8))
 
 (defsubst instagram--first-url (vec)
-  (alist-get 'url (aref vec 0)))
+  (gethash "url" (aref vec 0)))
 
 (defun instagram-item-image (item)
   (instagram--first-url
-   (let-alist item
+   (let-hash item
      .image_versions2.candidates)))
 
 (defun instagram-item-carousel (item)
-  (alist-get 'carousel_media item))
+  (gethash "carousel_media" item))
 
 (defun instagram-item-video (item)
   (when (instagram-item-video-p item)
-    (instagram--first-url (alist-get 'video_versions item))))
+    (instagram--first-url (gethash "video_versions" item))))
 
 (defun instagram-item-caption (item)
-  (let-alist item
+  (let-hash item
     (when .caption.text
       (xml-escape-string .caption.text))))
 
 (defun instagram-item-time (item)
-  (alist-get 'taken_at item))
+  (gethash "taken_at" item))
 
 (defun instagram-item-link (item)
-  (format "https://www.instagram.com/p/%s/" (alist-get 'code item)))
+  (format "https://www.instagram.com/p/%s/" (gethash "code" item)))
 
 (provide 'instagram)
 ;;; instagram.el ends here
