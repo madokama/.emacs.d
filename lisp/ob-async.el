3a76bd6b27de583dcf1a84f4d3efeb6c5cad22c4
Madoka Machitani <madokama@gmail.com>
2018-01-08 11:01:03 +0900

    lisp/ob-async.el: Support Windows command prompt.

--- lisp/ob-async.el
+++ lisp/ob-async.el
@@ -54,49 +54,50 @@
           (message "Executing async %s code block%s..."
                    lang
                    (if name (format " (%s)" name) ""))
-          (if (member lang '("sh"))
-              (org-babel-execute-async:shell lang info buf ov)
-            (async-start
-             `(lambda ()
-                ,(async-inject-variables
-                  (rx bos (or "load-path" "org-babel-library-of-babel") eos))
-                (require 'org)
-                (org-babel-do-load-languages 'org-babel-load-languages
-                                             ',org-babel-load-languages)
-                (with-current-buffer (find-file-noselect ,file)
-                  (goto-char ,(nth 5 info))
-                  (let ((org-confirm-babel-evaluate nil))
-                    (cons (org-babel-execute-src-block
-                           nil ',info
-                           ',(progn
-                               (nconc (assq :result-params params) '("silent"))
-                               params))
-                          (let ((ebuf (get-buffer org-babel-error-buffer-name)))
-                            (when ebuf
-                              (with-current-buffer ebuf
-                                (buffer-string))))))))
-             (pcase-lambda (`(,result . ,error))
-               (unwind-protect
-                    (with-selected-frame frame
-                      (cond (error
-                             (org-babel-eval-error-notify nil error))
-                            (result
-                             (unless quiet
-                               (with-current-buffer buf
-                                 (erase-buffer)
-                                 (insert (if (stringp result)
-                                             result
-                                           (pp-to-string result))))))
-                            ;; (t (kill-buffer buf))
-                            ))
-                 (delete-overlay ov))))
-            ;; Report progress
-            (unless quiet
-              (set-process-filter async--procvar
-                                  (lambda (proc output)
-                                    (org-babel--async-show-progress buf output)
-                                    (with-current-buffer (process-buffer proc)
-                                      (insert output)))))))
+          (pcase lang
+            ((or "sh" "cmd") (org-babel-execute-async:shell lang info buf ov))
+            (_
+             (async-start
+              `(lambda ()
+                 ,(async-inject-variables
+                   (rx bos (or "load-path" "org-babel-library-of-babel") eos))
+                 (require 'org)
+                 (org-babel-do-load-languages 'org-babel-load-languages
+                                              ',org-babel-load-languages)
+                 (with-current-buffer (find-file-noselect ,file)
+                   (goto-char ,(nth 5 info))
+                   (let ((org-confirm-babel-evaluate nil))
+                     (cons (org-babel-execute-src-block
+                            nil ',info
+                            ',(progn
+                                (nconc (assq :result-params params) '("silent"))
+                                params))
+                           (let ((ebuf (get-buffer org-babel-error-buffer-name)))
+                             (when ebuf
+                               (with-current-buffer ebuf
+                                 (buffer-string))))))))
+              (pcase-lambda (`(,result . ,error))
+                (unwind-protect
+                     (with-selected-frame frame
+                       (cond (error
+                              (org-babel-eval-error-notify nil error))
+                             (result
+                              (unless quiet
+                                (with-current-buffer buf
+                                  (erase-buffer)
+                                  (insert (if (stringp result)
+                                              result
+                                            (pp-to-string result))))))
+                             ;; (t (kill-buffer buf))
+                             ))
+                  (delete-overlay ov))))
+             ;; Report progress
+             (unless quiet
+               (set-process-filter async--procvar
+                                   (lambda (proc output)
+                                     (org-babel--async-show-progress buf output)
+                                     (with-current-buffer (process-buffer proc)
+                                       (insert output))))))))
         t))))
 
 ;;;###autoload(add-hook 'org-ctrl-c-ctrl-c-hook #'org-babel-execute-async)
@@ -104,6 +105,14 @@
 
 
 (require 'ob-shell)
+
+(defun ob-async--shell-cmdline (lang script)
+  (pcase lang
+    ("cmd"
+     (let ((bat (concat script ".bat")))
+       (rename-file script bat)
+       (list "cmd.exe" "/C" bat)))
+    ("sh" (list lang "--noediting" script))))
 
 (defun org-babel-execute-async:shell (lang info buf ov)
   (let* ((params (nth 2 info))
@@ -132,7 +141,7 @@
       (make-process :name "ob-sh"
                     :buffer buf
                     :stderr buf
-                    :command (list lang "--noediting" script)
+                    :command (ob-async--shell-cmdline lang script)
                     :filter (when buf
                               (lambda (_proc output)
                                 (org-babel--async-show-progress buf output)))
