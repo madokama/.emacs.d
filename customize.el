f6fcddbb2f5bc3a5759f5f5c389c26d85cb7a347
Madoka Machitani <madokama@gmail.com>
2018-01-21 08:49:17 +0900

    customize.el: Update bunch of unrelated settings.

--- customize.el
+++ customize.el
@@ -3,31 +3,31 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(async-bytecomp-allowed-packages (quote (all)))
- '(aw-keys (quote (97 115 100 102 103 104 106 107 108)))
+ '(async-bytecomp-allowed-packages '(all))
+ '(auth-source-cache-expiry nil)
+ '(aw-keys '(97 115 100 102 103 104 106 107 108))
  '(blink-cursor-mode nil)
  '(byte-compile-verbose nil)
  '(checkdoc-force-docstrings-flag nil)
  '(checkdoc-permit-comma-termination-flag t)
  '(checkdoc-verb-check-experimental-flag nil)
  '(company-backends
-   (quote
-    (company-semantic company-capf company-files
+   '(company-semantic company-capf company-files
      (company-dabbrev-code company-gtags company-keywords)
-     company-dabbrev)))
+     company-dabbrev))
  '(company-dabbrev-code-everywhere t)
- '(company-global-modes (quote (not shell-mode term-mode)))
+ '(company-global-modes '(not shell-mode term-mode))
  '(company-minimum-prefix-length 2)
  '(company-selection-wrap-around t)
  '(compilation-always-kill t)
  '(compilation-ask-about-save nil)
- '(confirm-kill-emacs (quote y-or-n-p))
- '(counsel-describe-function-preselect (quote ivy-function-called-at-point))
+ '(confirm-kill-emacs 'y-or-n-p)
+ '(counsel-describe-function-preselect 'ivy-function-called-at-point)
  '(counsel-find-file-at-point t)
  '(counsel-git-grep-skip-counting-lines nil)
  '(counsel-grep-base-command
    "rg -i -M 120 --no-heading --line-number --color never '%s' %s")
- '(counsel-grep-post-action-hook (quote (recenter)))
+ '(counsel-grep-post-action-hook '(recenter))
  '(counsel-mode-override-describe-bindings t)
  '(counsel-yank-pop-preselect-last t)
  '(create-lockfiles nil)
@@ -35,8 +35,7 @@
  '(custom-raised-buttons nil)
  '(custom-safe-themes t)
  '(default-frame-alist
-   (quote
-    ((internal-border-width . 0)
+   '((internal-border-width . 0)
      (scroll-bar-width . 0)
      (scroll-bar-height . 0)
      (horizontal-scroll-bars)
@@ -44,13 +43,12 @@
      (bottom-divider-width . 0)
      (background-mode . dark)
      (screen-gamma . 2.2)
-     (title))))
+     (title)))
  '(delete-by-moving-to-trash t)
  '(dired-dwim-target t)
  '(dired-listing-switches "-laGh1v --group-directories-first")
  '(display-buffer-alist
-   (quote
-    (("\\(?:\\*\\(?:\\(?:Async Shell Command\\|compilation\\)\\*\\)\\)" display-buffer-no-window)
+   '(("\\(?:\\*\\(?:\\(?:Async Shell Command\\|compilation\\)\\*\\)\\)" display-buffer-no-window)
      ("\\`\\*\\(?:eww\\)\\*"
       (display-buffer-reuse-window display-buffer-pop-up-window))
      ("\\`\\*\\(?:shell\\)\\*"
@@ -59,17 +57,17 @@
       (display-buffer-reuse-window display-buffer-in-side-window)
       (reusable-frames . visible)
       (side . bottom)
-      (window-height . 0.3)))))
+      (window-height . 0.3))))
  '(display-time-default-load-average nil)
  '(display-time-format " %H:%M")
  '(doc-view-continuous t)
  '(echo-keystrokes 0.1)
  '(ediff-diff-options "-w")
- '(ediff-split-window-function (quote split-window-horizontally))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(ediff-split-window-function 'split-window-horizontally)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(eldoc-idle-delay 0.8)
  '(elfeed-curl-timeout 60)
- '(elfeed-db-directory "~/Dropbox/emacs/elfeed")
+ '(elfeed-db-directory "~/.emacs.d/elfeed")
  '(elfeed-enclosure-default-dir "~/Downloads/")
  '(elfeed-show-truncate-long-urls nil)
  '(elfeed-use-curl nil)
@@ -81,15 +79,14 @@
  '(eshell-history-size 512)
  '(eww-history-limit 300)
  '(eww-search-prefix "https://www.google.com/search?q=")
- '(explicit-bash-args (quote ("--noediting" "--login" "-i")))
+ '(explicit-bash-args '("--noediting" "--login" "-i"))
  '(explicit-shell-file-name "bash")
  '(extempore-use-pretty-lambdas nil)
  '(fast-but-imprecise-scrolling t)
  '(fit-window-to-buffer-horizontally t)
- '(flycheck-emacs-lisp-load-path (quote inherit))
+ '(flycheck-emacs-lisp-load-path 'inherit)
  '(flycheck-global-modes
-   (quote
-    (not org-mode elfeed-search-mode elfeed-show-mode diff-mode)))
+   '(not org-mode elfeed-search-mode elfeed-show-mode diff-mode))
  '(flycheck-keymap-prefix "f")
  '(flycheck-mode-line-prefix "")
  '(flycheck-pos-tip-timeout 5)
@@ -107,108 +104,111 @@
  '(initial-scratch-message nil)
  '(ivy-auto-select-single-candidate t t)
  '(ivy-completing-read-handlers-alist
-   (quote
-    ((tmm-menubar . completing-read-default)
+   '((tmm-menubar . completing-read-default)
      (tmm-shortcut . completing-read-default)
      (image-save . completing-read-default)
-     (open-junk-file . completing-read-default))))
- '(ivy-extra-directories (quote ("./")))
+     (open-junk-file . completing-read-default)))
+ '(ivy-extra-directories '("./"))
  '(ivy-fixed-height-minibuffer nil)
- '(ivy-format-function (quote ivy-format-function-arrow))
+ '(ivy-format-function 'ivy-format-function-arrow)
  '(ivy-ignore-buffers
-   (quote
-    ("\\` " "company-statistics-cache\\.el" "/elfeed/index" "~\\'")))
- '(ivy-magic-slash-non-match-action (quote ivy-magic-slash-non-match-create))
+   '("\\` " "company-statistics-cache\\.el" "/elfeed/index" "~\\'"))
+ '(ivy-magic-slash-non-match-action 'ivy-magic-slash-non-match-create)
  '(ivy-use-selectable-prompt t)
  '(ivy-use-virtual-buffers t)
- '(ivy-virtual-abbreviate (quote full))
+ '(ivy-virtual-abbreviate 'full)
  '(ivy-wrap nil)
  '(jit-lock-stealth-load 200)
  '(jit-lock-stealth-time 1)
  '(kill-do-not-save-duplicates t)
  '(kill-whole-line t)
  '(large-file-warning-threshold 536870911)
- '(lisp-indent-function (quote common-lisp-indent-function))
- '(lispy-avy-style-symbol (quote at-full))
+ '(lisp-indent-function 'common-lisp-indent-function)
+ '(lispy-avy-style-symbol 'at-full)
  '(lispy-comment-use-single-semicolon t)
  '(lispy-no-space t)
  '(ls-lisp-use-insert-directory-program t)
- '(mailcap-user-mime-data (quote ((pdf-view-mode "application/pdf" nil))))
+ '(mailcap-user-mime-data '((pdf-view-mode "application/pdf" nil)))
  '(menu-bar-mode nil)
+ '(mpa-options '("--volume=80" "--ytdl-raw-options=no-mark-watched="))
  '(multi-term-program "bash")
  '(multi-term-program-switches "--login")
- '(next-error-recenter (quote (4)))
+ '(next-error-recenter '(4))
  '(next-screen-context-lines 1)
- '(org-agenda-files (quote ("~/Dropbox/emacs/org/notes.org")))
- '(org-babel-clojure-backend (quote cider))
+ '(org-agenda-files '("~/Dropbox/emacs/org/notes.org"))
+ '(org-babel-clojure-backend 'cider)
  '(org-babel-hash-show-time t)
- '(org-babel-shell-names (quote ("sh" "bash")))
+ '(org-babel-shell-names '("sh" "bash" "cmd" "bat"))
  '(org-capture-templates
-   (quote
-    (("l" "Link" entry
-          (file+olp+datetree "")
-          "* %:description  %^g
-%(org-capture--quote)%?Source: %:link
-%U")
+   '(("p" "Protocol" entry
+      (file+olp+datetree "")
+      "* %^{Title}
+Source: %u, %c
+ #+BEGIN_QUOTE
+%i
+#+END_QUOTE
+%?")
+     ("L" "Protocol Link" entry
+      (file+olp+datetree "")
+      "* %(org-make-link-string \"%:link\" \"%:description\")
+%?
+Captured On: %U")
      ("y" "YouTube" entry
-          (file+olp+datetree "vids.org")
-          "* %(org-ytdl-capture)  %^g
+      (file+olp+datetree "vids.org")
+      "* %(org-ytdl-capture)  %^g
 %(org-ytdl-run-template-hook)
 %?
 %U")
      ("t" "Todo" entry
-          (file+olp+datetree "todo.org")
-          "* TODO %?
+      (file+olp+datetree "todo.org")
+      "* TODO %?
 %i
 %a
 %U")
      ("n" "Note" entry
-          (file+olp+datetree "")
-          "* %?
+      (file+olp+datetree "")
+      "* %?
 %i
 %a
-%U"))))
- '(org-catch-invisible-edits (quote show-and-error))
+%U")))
+ '(org-catch-invisible-edits 'show-and-error)
  '(org-confirm-babel-evaluate nil)
  '(org-directory "~/Dropbox/emacs/org")
  '(org-ellipsis "…")
  '(org-hide-emphasis-markers nil)
  '(org-image-actual-width nil)
  '(org-protocol-default-template-key "l")
- '(org-refile-targets (quote ((org-agenda-files :maxlevel . 6))))
+ '(org-refile-targets '((org-agenda-files :maxlevel . 6)))
  '(org-return-follows-link t)
  '(org-src-fontify-natively t)
  '(org-src-preserve-indentation t)
- '(org-src-window-setup (quote current-window))
+ '(org-src-window-setup 'current-window)
  '(org-startup-indented t)
  '(org-startup-with-inline-images t)
  '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.org/packages/"))))
+   '(("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")))
  '(package-enable-at-startup nil)
- '(pdf-tools-enabled-hook (quote (pdf-view-midnight-minor-mode auto-revert-mode)))
+ '(pdf-tools-enabled-hook '(pdf-view-midnight-minor-mode auto-revert-mode))
  '(pdf-tools-enabled-modes
-   (quote
-    (pdf-history-minor-mode pdf-isearch-minor-mode pdf-links-minor-mode pdf-outline-minor-mode pdf-cache-prefetch-minor-mode pdf-view-auto-slice-minor-mode pdf-occur-global-minor-mode)))
+   '(pdf-history-minor-mode pdf-isearch-minor-mode pdf-links-minor-mode pdf-outline-minor-mode pdf-cache-prefetch-minor-mode pdf-view-auto-slice-minor-mode pdf-occur-global-minor-mode))
  '(pdf-tools-handle-upgrades nil)
  '(pdf-view-use-imagemagick t)
  '(persistent-scratch-backup-directory "~/Dropbox/emacs/.persistent-scratch")
- '(persistent-scratch-what-to-save (quote (major-mode point)))
- '(powerline-default-separator (quote chamfer))
+ '(persistent-scratch-what-to-save '(major-mode point))
+ '(powerline-default-separator 'chamfer)
  '(proced-auto-update-flag t)
  '(proced-tree-flag t)
- '(recentb-abema-channels (quote (shogi)))
- '(recenter-positions (quote (top middle bottom)))
+ '(recentb-abema-channels '(shogi))
+ '(recenter-positions '(top middle bottom))
  '(recentf-auto-cleanup 300)
- '(recentf-exclude (quote ("~\\'" "\\.elc\\'" "/\\.emacs\\.d/url/cache/")))
+ '(recentf-exclude '("~\\'" "\\.elc\\'" "/\\.emacs\\.d/url/cache/"))
  '(recentf-max-saved-items 999)
  '(require-final-newline t)
- '(ring-bell-function (quote ignore))
+ '(ring-bell-function 'ignore)
  '(safe-local-variable-values
-   (quote
-    ((coding-system-for-read . utf-8)
-     (c-default-style . "gnu"))))
+   '((coding-system-for-read . utf-8)
+     (c-default-style . "gnu")))
  '(save-interprogram-paste-before-kill t)
  '(save-place-forget-unreadable-files t)
  '(save-place-ignore-files-regexp "\\(?:COMMIT_EDITMSG\\|/elfeed/index\\)$")
@@ -217,30 +217,27 @@
  '(sentence-end-double-space nil)
  '(shell-completion-execonly nil)
  '(shell-dynamic-complete-functions
-   (quote
-    (comint-c-a-p-replace-by-expanded-history shell-environment-variable-completion shell-command-completion shell-c-a-p-replace-by-expanded-directory shell-filename-completion comint-filename-completion)))
+   '(comint-c-a-p-replace-by-expanded-history shell-environment-variable-completion shell-command-completion shell-c-a-p-replace-by-expanded-directory shell-filename-completion comint-filename-completion))
  '(shell-file-name "sh")
  '(shr-image-animate t)
  '(shr-width 80)
  '(so-long-minor-modes
-   (quote
-    (font-lock-mode highlight-changes-mode hi-lock-mode hl-line-mode linum-mode nlinum-mode prettify-symbols-mode visual-line-mode whitespace-mode rainbow-delimiters-mode show-paren-mode electric-indent-mode)))
- '(so-long-target-modes (quote (prog-mode css-mode sgml-mode nxml-mode org-mode)))
+   '(font-lock-mode highlight-changes-mode hi-lock-mode hl-line-mode linum-mode nlinum-mode prettify-symbols-mode visual-line-mode whitespace-mode rainbow-delimiters-mode show-paren-mode electric-indent-mode))
+ '(so-long-target-modes '(prog-mode css-mode sgml-mode nxml-mode org-mode))
  '(swiper-action-recenter t)
- '(switch-to-buffer-in-dedicated-window (quote prompt))
+ '(switch-to-buffer-in-dedicated-window 'prompt)
  '(symon-refresh-rate 2)
- '(symon-sparkline-ascent (quote center))
+ '(symon-sparkline-ascent 'center)
  '(symon-sparkline-thickness 1)
- '(symon-sparkline-type (quote plain))
+ '(symon-sparkline-type 'plain)
  '(symon-sparkline-width 72)
- '(tab-always-indent (quote complete))
+ '(tab-always-indent 'complete)
  '(tao-theme-use-height nil)
  '(tool-bar-mode nil)
  '(undo-tree-incompatible-major-modes
-   (quote
-    (magit-status-mode magit-process-mode magit-diff-mode messages-buffer-mode term-mode shell-mode elfeed-search-mode elfeed-show-mode Custom-mode eww-mode)))
+   '(magit-status-mode magit-process-mode magit-diff-mode messages-buffer-mode term-mode shell-mode elfeed-search-mode elfeed-show-mode Custom-mode eww-mode))
  '(undo-tree-mode-lighter "")
- '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(uniquify-buffer-name-style 'forward nil (uniquify))
  '(url-cache-expire-time 604800)
  '(url-history-file "~/.emacs.d/url/history")
  '(url-mime-language-string "en-US,en")
