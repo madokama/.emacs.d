216f372a2b0a99cff2ad1292691e79658495652b
Madoka Machitani <madokama@gmail.com>
2018-02-24 19:37:19 +0900

    lisp/mpa.el: Major rewrite.

--- lisp/mpa.el
+++ lisp/mpa.el
@@ -7,7 +7,7 @@
 (eval-when-compile
   (require 'subr-x))
 (require 'seq)
-(require 'multi-term)
+(require 'mpv-ipc)
 
 (defgroup mpa ()
   "Audio player interface for mpv."
@@ -22,99 +22,64 @@
   "Command-line options to pass to mpv."
   :type '(repeat string))
 
+(defvar mpa-ipc nil)
+
 (defvar mpa-scrapers-alist nil)
 
 ;;;###autoload
 (defun mpa-play (lst)
-  (mpa-stop)
-  (let ((proc
-         (with-current-buffer
-             (make-term "mpa" multi-term-program nil multi-term-program-switches)
-           (multi-term-internal)
-           (get-buffer-process (current-buffer)))))
-    (process-send-string proc
-                         (concat
-                          (string-join
-                           `("mpv" ,@(when mpa-shuffle '("--shuffle"))
-                                   ,@mpa-options
-                                   "--no-video" "--keep-open=no"
-                                   ;; "--msg-level=all=no,ytdl_hook=info"
-                                   ,@(mpa-scrape lst))
-                           " ")
-                          "\n"))
-    (set-process-filter proc #'mpa--filter)))
+  (unless (process-live-p mpa-ipc)
+    (setq mpa-ipc
+          (mpv-ipc-watch `("--no-video" "--keep-open=yes"
+                           ,@(when mpa-shuffle '("--shuffle"))
+                           ,@mpa-options)
+                         #'mpa--filter))
+    (mpv-ipc:request-log mpa-ipc))
+  (mpv-ipc:load mpa-ipc (mpa-scrape lst)))
 
-(defun mpa--filter (_proc output)
-  (save-match-data
-    (cond ((string-match (rx "Volume: " (group (+ digit))) output)
-           (message "[mpa] vol: %s" (match-string 1 output)))
-          ((string-match (rx "[ytdl_hook] " (group (+? anything)) "[0K")
-                         output)
-           (message "[mpa] %s"
-                    (decode-coding-string
-                     (replace-regexp-in-string (rx (or "[0K" "\r" "\n"))
-                                               ""
-                                               (match-string 1 output))
-                     locale-coding-system))))))
-
-(defun mpa--current-process ()
-  (car
-   (cl-delete-if-not (lambda (p)
-                       (and (string-match-p (rx bos "mpa" eow) (process-name p))
-                            (eq (process-status p) 'run)))
-                     (process-list))))
-
-(defun mpa--send-key (key)
-  "Send input KEY to the mpa process.
-Return KEY if successfull, nil otherwise."
-  (when-let* ((p (mpa--current-process)))
-    (with-current-buffer (process-buffer p)
-      (term-send-raw-string key))
-    key))
+(defun mpa--filter (tag json)
+  (let-hash json
+    (cond (.event
+           (pcase .event
+             ("log-message"
+              (pcase .prefix
+                ("ytdl_hook"
+                 (message "[mpa] %s" (string-trim-right .text)))))))
+          (.request_id
+           (throw tag .data)))))
 
 (defun mpa-interact ()
   (cl-block nil
     (while t
       (if-let* ((char (read-char "mpa>")))
           (progn
-            (mpa--send-key (string char))
+            (mpv-ipc:keypress mpa-ipc (string char))
             (when (memq char '(?q ?Q))
               (cl-return)))
         (cl-return)))))
 
 (defun mpa-next ()
-  (mpa--send-key ">"))
+  (mpv-ipc:next mpa-ipc))
 
 (defun mpa-pause ()
-  (mpa--send-key " "))
+  (mpv-ipc:pause mpa-ipc))
 
 (defun mpa-volume-down ()
-  (mpa--send-key "/"))
+  (mpv-ipc:volume mpa-ipc -2))
 
 (defun mpa-volume-up ()
-  (mpa--send-key "*"))
+  (mpv-ipc:volume mpa-ipc 2))
 
 (defun mpa-loop ()
-  (mpa--send-key "L"))
+  (mpv-ipc:loop mpa-ipc))
 
 (defun mpa-stop ()
-  (when-let* ((p (mpa--current-process)))
-    (delete-process p)
-    (kill-buffer (process-buffer p))))
+  (mpv-ipc:stop mpa-ipc))
 
 (declare-function org-make-link-string "org")
 
-(defun mpa-recv-url (send)
-  (let ((kill-ring nil))
-    (and (funcall send "y")
-         (cl-loop for i from 10 downto 0
-                  do (sleep-for 0.5)
-                  if (ignore-errors (current-kill 0))
-                    return (current-kill 0)
-                  else if (zerop i) return nil))))
-
 (defun mpa-now-playing ()
-  (when-let* ((url (mpa-recv-url #'mpa--send-key)))
+  (when-let* ((url (mpv-ipc:get-path mpa-ipc)))
     (let-hash (ytdl-get-json url)
       (insert "\n"
               (org-make-link-string url .title)
