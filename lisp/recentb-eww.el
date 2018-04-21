aba7023bbe6027bc162b90046db3de29b03eadf9
Madoka Machitani <madokama@gmail.com>
2018-04-21 12:50:25 +0900

    lisp/recentb-eww.el: Sync with eww history in real time.

--- lisp/recentb-eww.el
+++ lisp/recentb-eww.el
@@ -5,16 +5,20 @@
 ;;; Code:
 
 (require 'recentb)
+(require 'eww-savehist)
 
 (recentb-define-mode eww-mode
   :var recentb-eww
   :history recentb-eww-history
   :candidate recentb-eww-candidate)
 
-(autoload 'eww-savehist-prune "eww-savehist")
-
 (defun recentb-eww-history ()
   (eww-savehist-prune))
+
+(defun recentb-eww-sync-history ()
+  (setq recentb-eww eww-savehist))
+
+(add-hook 'eww-after-render-hook #'recentb-eww-sync-history t)
 
 (defun recentb-eww-candidate (item)
   (let ((url (plist-get item :url)))
